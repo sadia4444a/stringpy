793b63102d253bd55e44137ded98ecaef5a79c9c
Mst. Sadia khatun <73788572+sadia4444a@users.noreply.github.com>
2021-05-14 21:19:18 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,38 @@
+print('hello world')
+
+massage ="""bobby's world dsjhddjikw
+sdvjhdgygdwui
+dhgqeuwdyi"""
+print(massage)
+
+massage2='hello world'
+print(massage2)
+print(len(massage2))
+print(massage2.upper())
+print(massage2.lower())
+print(massage2.count('hello'))
+print(massage2.find('world'))
+print(massage2[0:5])
+print(massage2.replace('world','univarse'))
+
+greatting='Hello'
+name='sadia'
+massage5=greatting +','+name +'.welcome!'
+print(massage5)
+massage5='{},{}.welcome!'.format(greatting,name)
+massage5=f'{greatting},{name.upper()}.welcome!'
+print(massage5)
+
+
+massage3="sadia's world"
+print(massage3)
+
+massage4='sadia\'s world'
+print(massage4)
+print(dir(name))
+print(help(str.lower))
+print(help(str))
+
+
+
+
